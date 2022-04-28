d06e776328c6da4e43ac617fb2d2d622c1e089a4
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-04-28 22:56:23 +0530

    Learning

--- formin.js
+++ formin.js
@@ -5,17 +5,25 @@ function getData()
     var dob = document.getElementById("dob").value;
     var email = document.getElementById("email").value;
     var salary = document.getElementById("salary").value; 
-    var gender= document.getElementById("gender").value; 
+    var radios = document.getElementsByName("gender"); 
     var car = document.getElementById("car").value; 
     var avail = document.getElementById("avail").checked;
     var essay = document.getElementById("essay").value; 
+    var gender = "Not Selected";
     //saving the values in local storage
+
     sessionStorage.setItem("name", name);
     sessionStorage.setItem("dob", dob);
     sessionStorage.setItem("email", email);
     sessionStorage.setItem("salary", salary);
-    sessionStorage.setItem("gender", gender);
     sessionStorage.setItem("car", car);
     sessionStorage.setItem("avail", avail);
     sessionStorage.setItem("essay", essay);
+    //accessing a radio type data
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            gender = radios[i].value
+        }
+    }
+    sessionStorage.setItem("gender", gender);
 }
