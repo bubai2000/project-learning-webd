d450a00260af8c63aa300d00d08c09ae86cc4ee7
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-05-20 22:54:43 +0530

    events

---
+++ jsevents.js
@@ -0,0 +1,12 @@
+function toggle()
+{
+    let btn = document.getElementById('btn');
+    let para = document.getElementById('para');
+    if(para.style.display != 'none')
+    {
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
