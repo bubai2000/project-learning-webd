ac0a0f61f199517a913c9c6f2adbafde99ca1a82
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-06-13 08:23:22 +0530

    Update

--- jsevents.js
+++ jsevents.js
@@ -1,12 +1,23 @@
 function toggle()
-{
-    let btn = document.getElementById('btn');
-    let para = document.getElementById('para');
-    if(para.style.display != 'none')
     {
-        para.style.display = 'none';
+        let btn = document.getElementById('btn');
+        let para = document.getElementById('para');
+        if(para.style.display != 'none')
+        {
+            para.style.display = 'none';
+        }
+        else{
+            para.style.display = 'block';
+        }
     }
-    else{
-        para.style.display = 'block';
-    }
+function hello()
+{
+    let para = document.getElementById('para');
+    para.addEventListener('mouseover', function fun(){
+        alert("Hi!")
+    });
+}
+function test(){
+    hello();
+    toggle();
 }
