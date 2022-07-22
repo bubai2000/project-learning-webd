3944851f6c595689416cc7cfeeca75a385d9b7b2
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-07-22 08:26:10 +0530

    Just added some more fun

--- timeout.js
+++ timeout.js
@@ -1,16 +1,17 @@
 let x = 0;
 let interval1 = setInterval(function(){
-    document.getElementById("text").textContent="You are on this page for "+x+" seconds(Please don't stay for more than 10 seconds)";
+    document.getElementById("text").textContent="You are on this page for "+x+" seconds(Wait for 10 seconds a magic will happen!)";
     x = x+1;
 },1000) //Automatically runs in cycle after every given interval
 
 setTimeout(function(){
     clearInterval(interval1);
     test();
+    rickroll();
 },11000) //Runs for one time after given interval
 
 function test(){
-    document.getElementById("text").textContent="Now you will be punished for not listening to my words!!!";
+    document.getElementById("text").textContent="Surprise";
     document.getElementById("text").style.color="red";   
 }
 
@@ -19,3 +20,9 @@ setInterval(()=>{ //using arrow function
     this.time = date.getHours() +":" +date.getMinutes()+":"+date.getSeconds();
     document.getElementById("time").textContent="Current time is "+ time;
 },1000)
+function rickroll(){
+    var temp = '<video controls src="rick.mp4" autoplay></video>';
+    var a = document.createElement("VIDEO");
+    a.innerHTML = temp;
+    document.getElementById('rickroll').appendChild(a.childNodes[0]);
+}
