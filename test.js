88015d89b0b6066cb8dc4312876d24eb8258698e
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-07-29 10:45:02 +0530

    Test

--- test.js
+++ test.js
@@ -1,25 +1,7 @@
-let a=11;
-function evenwarn(a){
-    let error=false;
-    if(a%2==0){
-        error=true;
-    }
-    return new Promise(function(resolve,reject){
-        setTimeout(()=>{
-            if(!error){
-                console.log(a+" is not an even number!");
-                resolve();
-            }
-            else{
-                console.log(a+" is an even number!");
-                reject("Your entered number is even!");
-            }
-        },2000);
-    })
-}
-
-evenwarn(a).then(function(){
-    console.log("Thanks for entering an odd number!");
-}).catch(function(error){
-    console.log("Marks deducted! Reason: "+error);
-})
+import $ from 'jquery';
+$.get(
+  "https://62e27b1ce8ad6b66d85c7012.mockapi.io/test/testuser",
+  function (data) {
+    console.log(data);
+  }
+);
